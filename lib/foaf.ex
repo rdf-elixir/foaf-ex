a83c28dda85c6c37f72b31e459f7dd4c92d5a0d9
Marcel Otto <marcelotto@gmx.de>
2024-08-07 13:00:24 +0200

    Add docs

--- lib/foaf.ex
+++ lib/foaf.ex
@@ -2,6 +2,11 @@ defmodule FOAF do
   @moduledoc """
   Implementation of the FOAF vocabulary.
 
+  The following features are provided:
+
+  - The `FOAF` module acts as the `RDF.Vocabulary.Namespace` for the FOAF vocabulary.
+  - Grax structures for all FOAF classes and their properties.
+
   See <http://xmlns.com/foaf/0.1/>.
   """
 
