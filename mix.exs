6bced2e2ead866e7e1ca170bbe96c6691fd9e9d7
Marcel Otto <marcelotto@gmx.de>
2024-08-07 12:52:24 +0200

    Add ExCoveralls config

--- mix.exs
+++ mix.exs
@@ -15,8 +15,15 @@ defmodule FOAF.MixProject do
 
       # Dialyzer
       dialyzer: dialyzer(),
+
+      # ExCoveralls
+      test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        check: :test
+        check: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
       ]
     ]
   end
